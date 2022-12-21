c371eb0ae6c5eb123e8937494794e29d1270c720
Eric Passmore <eric.passmore@gmail.com>
2022-12-20 18:52:52 -0800

    updated cypress tests

--- testing/cypress.config.js
+++ testing/cypress.config.js
@@ -1,10 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'antelopeIO-docsgen',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    }
+  },
+  env: {
+      proto: 'http',
+      host: 'localhost:3000',
+      contracts_repo: 'reference-contracts'
   },
   video: false,
 });
